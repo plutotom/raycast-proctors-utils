c01c2358391da17d1d15b17c5eb12c31df95feb1
Isaiah Proctor <isaiahd.proctor@gmail.com>
2025-04-01 18:29:15 -0500

    added script back in

---
+++ src/dismiss-notifications.ts
@@ -0,0 +1,22 @@
+import { showHUD, Clipboard } from "@raycast/api";
+import { runAppleScript } from "@raycast/utils";
+
+export default async function main() {
+  runAppleScript(`
+    tell application "System Events" to tell application process "NotificationCenter"
+    try
+      repeat with uiElement in (actions of UI elements of scroll area 1 of group 1 of group 1 of window "Notification Center" of application process "NotificationCenter" of application "System Events")
+        if description of uiElement contains "Close" then
+          perform uiElement
+        end if
+        if description of uiElement contains "Clear" then
+          perform uiElement
+        end if
+      end repeat
+    return ""
+    end try
+  end tell
+  `);
+
+  await showHUD("Dismissed notifications");
+}
