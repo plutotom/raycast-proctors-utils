dcbbb3b179d0ac4883a1848487ca6f835033e37c
Isaiah Proctor <isaiahd.proctor@gmail.com>
2025-04-02 11:00:36 -0500

    tryed to fix info from apple script

--- src/dismiss-notifications.ts
+++ src/dismiss-notifications.ts
@@ -1,23 +1,27 @@
 import { showHUD } from "@raycast/api";
-import { runAppleScript } from "@raycast/utils";
+import { runAppleScript, showFailureToast } from "@raycast/utils";
 export default async function main() {
-  runAppleScript(`
-    tell application "System Events" to tell application process "NotificationCenter"
-	try
-		repeat with uiElement in (actions of UI elements of scroll area 1 of group 1 of group 1 of window "Notification Center" of application process "NotificationCenter" of application "System Events")
-			if description of uiElement contains "Close" then
-				perform uiElement
-			end if
-			if description of uiElement contains "Clear" then
-				perform uiElement
-			end if
-			if description of uiElement contains "Clear All" then
-				perform uiElement
-			end if
-		end repeat
-		return ""
-	end try
-end tell
-  `);
-  await showHUD("Dismissed notifications");
+  try {
+    const res = await runAppleScript(`
+	    tell application "System Events" to tell application process "NotificationCenter"
+		try
+			repeat with uiElement in (actions of UI elements of scroll area 1 of group 1 of group 1 of window "Notification Center" of application process "NotificationCenter" of application "System Events")
+				if description of uiElement contains "Close" then
+					perform uiElement
+				end if
+				if description of uiElement contains "Clear" then
+					perform uiElement
+				end if
+				if description of uiElement contains "Clear All" then
+					perform uiElement
+				end if
+			end repeat
+			return ""
+		end try
+	end tell
+	  `);
+    await showHUD("Dismissed notifications" + res);
+  } catch (error) {
+    showFailureToast(error, { title: "Could not run AppleScript" });
+  }
 }
